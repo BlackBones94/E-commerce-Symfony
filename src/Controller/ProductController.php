aa9a1971ae1255a180f050aad1d021cbcc744c5b
DESKTOP-BIPV4FE\Damien Pellet <damien.pellet94@hotmail.fr>
2024-03-13 10:58:13 +0100

    form

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findOneBy([
+            'slug' => $slug,
+        ]);
+
+        if (!$category) {
+            throw $this->createNotFoundException("La catégory demander n'existe pas");
+        }
+
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category
+
+        ]);
+    }
+
+
+    /**
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+    public function show($slug, ProductRepository $productRepository)
+    {
+
+        $product = $productRepository->findOneBy([
+            'slug' => $slug,
+        ]);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit demandé n'existe pas");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}/edit", name="product_edit")
+     */
+    public function edit($id, ProductRepository $productRepository, Request $request, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator)
+    {
+        $product = $productRepository->find($id);
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $formView = $form->createView();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'formView' => $formView
+        ]);
+    }
+
+
+    /**
+     *@Route("/admin/product/create" , name="product_create") 
+     */
+    public function create(FormFactoryInterface $factory, Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+
+        $form = $this->createForm(ProductType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $product = $form->getData();
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+
+            $em->persist($product);
+            $em->flush();
+
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
+        ]);
+    }
+}
---
+++ src/Form/ProductType.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\DataTransformer\CentimesTransformer;
+use App\Form\Type\PriceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du produit',
+                'attr'  => [
+                    'placeholder' => 'Tapez le nom du produit'
+                ]
+            ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr'  =>  [
+                    'placeholder' => 'Tapez la description du produit'
+                ]
+            ])
+
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du produit ',
+                'attr'  => [
+                    'placeholder' => 'Tapez le prix du produit en €'
+                ],
+                'divisor' => 100
+            ])
+
+            ->add('mainPicture', UrlType::class, [
+                'label' => 'image du produit',
+                'attr' => ['placeholder' => 'Tapez une URL d\'image !']
+            ])
+
+
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
+                'placeholder' => '-- Choisir une catégorie --',
+                'class' => Category::class,
+                'choice_label' => function (Category $category) {
+                    return strtoupper($category->getName());
+                }
+            ]);
+
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event){
+        //     $product = $event->getData();
+
+        //     if($product->getPrice() !== null){
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });  
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+        //     $form = $event->getForm();
+
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if($product->getPrice() !== null){
+        //     $product->setPrice($product->getPrice() / 100);
+        //     }
+        // });
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
