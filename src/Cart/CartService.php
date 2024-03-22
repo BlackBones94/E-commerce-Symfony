118b380e41eb58fd5d1b948c67e6e19be8dcd5bb
DESKTOP-BIPV4FE\Damien Pellet <damien.pellet94@hotmail.fr>
2024-03-22 11:31:02 +0100

    add service panier

---
+++ src/Cart/CartService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Cart;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class CartService{
+
+    protected $session;
+    protected $productRepository;
+
+    public function __construct(SessionInterface $session, ProductRepository $productRepository)
+    {
+        $this->session = $session;
+        $this->productRepository = $productRepository;
+    }
+
+    protected function getCart() : array {
+        return $this->session->get('cart', []);
+    }
+
+    protected function saveCart(array $cart){
+        $this->session->set('cart' , $cart);
+    }
+    
+    public function add(int $id){
+        $cart = $this->getCart();
+
+        if (!array_key_exists($id, $cart)) {
+            $cart[$id] = 0;
+        } 
+
+        $cart[$id]++;
+        
+        $this->saveCart($cart);
+    }
+
+
+    public function remove(int $id){
+        $cart = $this->getCart();
+
+        unset($cart[$id]);
+
+        $this->saveCart($cart);
+    }
+
+    public function decrement(int $id){
+        $cart = $this->getCart();
+
+        if(!array_key_exists($id, $cart)){
+            return; 
+        }
+
+        if($cart[$id] === 1){
+            $this->remove($id);
+            return;
+        }
+
+        $cart[$id]--;
+
+        $this->saveCart($cart);
+    }
+
+
+    public function getTotal() : int 
+    {
+        $total = 0;
+
+        foreach ($this->getCart() as $id => $qty) {
+            $product = $this->productRepository->find($id);
+
+            if(!$product) {
+                continue;
+            }
+           
+            $total += $product->getPrice() * $qty;
+        }
+
+        return $total;
+    }
+
+    public function getDetailedCartItems() : array 
+    {
+        $detailedCart = [];
+        
+
+        foreach ($this->getCart() as $id => $qty) {
+            $product = $this->productRepository->find($id);
+
+            if(!$product){
+                continue;
+            }
+
+            $detailedCart[] = new CartItem($product, $qty);
+        }
+
+        return $detailedCart;
+    }
+}
